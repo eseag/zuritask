e8b0e8b94151aa31174895411503989cf75ef7af
eseag <agadagbaesefaith@gmail.com>
2022-06-25 13:12:31 +0100

    first commit

---
+++ javaislife.js
@@ -0,0 +1,12 @@
+
+    let x,y;
+    x = parseFloat(parseInt(prompt ('enter the frist number')));
+    y = parseFloat(parseInt(prompt ('enter the second number')));
+    let a = x+y;
+    let s = x-y
+    let d = x/y
+    let m = x*y
+    alert ('the sum of the given numbers is:' +a)
+    alert ('the difference of the given numbers is:'+s)
+    alert ('the quotient of the given numbers is:' +d)
+    alert ('the product of the given numbers is:' +m)
